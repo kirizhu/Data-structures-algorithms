bbc798aa44840f7afee5ab4a57178289cfc1e3c9
KiriZhu <kiribaty88@gmail.com>
2020-11-04 11:15:07 +0100

    BigO rules and exercises  up to 3

---
+++ Big-O/BigO-Exercise-03-Rule3.js
@@ -0,0 +1,10 @@
+const compressBoxesTwice = (boxes, boxes 2) => {
+  boxes.forEach((box) => {
+    console.log(box);
+  });
+  boxes2.forEach((box) => {
+    console.log(box);
+  });
+};
+//O(a+b)
+// Apply rule 3
